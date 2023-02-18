6f869038457dc8c2ff18a361bd352f0aa9bd5e5f
Moammad Farhad <mfarhadattari>
2023-02-18 09:51:55 +0600

    geomertry calculation function

---
+++ js/geometry-calculation.js
@@ -0,0 +1,14 @@
+function areaOfTriangle(b, h){
+    const area = 0.5 * b * h ;
+    return area ;
+}
+
+function areaOfRectangle(w, l){
+    const area = w * l ;
+    return area ;
+}
+
+function areaOfEllipse(a, b){
+    const area = Math.PI * a * b ;
+    return area ;
+}
--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ function getInputFieldValueById(inputFieldId){
     const inputField = document.getElementById(inputFieldId) ;
     const inputFieldValueString = inputField.value ;
     const inputFieldValue = parseFloat(inputFieldValueString) ;
+    return inputFieldValue ;
 }
 
 function setTextElementValueById(elementId){
