1027d84262bc0b2df70a601c77c9eb37b0aa170d
Moammad Farhad <mfarhadattari>
2023-02-18 09:44:52 +0600

    getInputFieldValueById() and setTextElementValueById() function declered

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.getElementById('blog-btn').addEventListener('click', function(){
     window.location.href = 'blog.html' ;
 })
+
+
+function getInputFieldValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId) ;
+    const inputFieldValueString = inputField.value ;
+    const inputFieldValue = parseFloat(inputFieldValueString) ;
+}
+
+function setTextElementValueById(elementId){
+    const element = document.getElementById(elementId) ;
+    element.innerText = element ;
+}
