f3a4c7be1b246f0cd5c854b492b056cf37af4453
Moammad Farhad <mfarhadattari>
2023-02-18 10:24:32 +0600

    rectangular area calculation complete

--- js/geometry-calculation.js
+++ js/geometry-calculation.js
@@ -1,8 +1,20 @@
+// Triangular Area Calculation
 document.getElementById('triangle-calculate-btn').addEventListener('click', function(){
     const width = getInputFieldValueById('triangle-width-field') ;
     const hight = getInputFieldValueById('triangle-hight-field') ;
     setTextElementValueById('triangle-width', width) ;
     setTextElementValueById('triangle-hight', hight) ;
     const area = areaOfTriangle(width , hight) ;
-    console.log(area)
+    console.log(area) ;
+})
+
+
+// Rectangular Area Calculation
+document.getElementById('rectangle-calculate-btn').addEventListener('click', function(){
+    const width = getInputFieldValueById('rectangle-width-field') ;
+    const length = getInputFieldValueById('rectangle-length-field') ;
+    setTextElementValueById('rectangle-width', width) ;
+    setTextElementValueById('rectangle-length', length) ;
+    const area = areaOfRectangle(width , length) ;
+    console.log(area) ;
 })
